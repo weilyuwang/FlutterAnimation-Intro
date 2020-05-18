638846b05e35b493688e3fdfc775c7d3b7571345
weilyuwang <weilyuwang@gmail.com>
2020-05-18 16:41:45 -0400

    add initial starter files

---
+++ android/app/src/main/kotlin/com/example/animation_intro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.animation_intro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:animation_intro/src/app.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
